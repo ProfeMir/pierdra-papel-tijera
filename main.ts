a3e8eac9b9c9d00a588f5cdcbf3bba58bb825fd4
ProfeMir <90851290+ProfeMir@users.noreply.github.com>
2021-09-16 15:06:39 -0300

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,31 @@
+let MANO = 0
+input.onGesture(Gesture.Shake, function () {
+    MANO = randint(1, 3)
+    if (MANO == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # # # .
+            # # # # #
+            . . . . .
+            `)
+    }
+    if (MANO == 2) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+    if (MANO == 3) {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
